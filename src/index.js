50017ecda7d32c598f1265d70d6ae0dcc23d1d28
ViktorKrasnorutskiy <viktor.krasnorutskiy@yandex.ru>
2020-02-24 04:20:44 +0500

    add about self content and background
    1. Текст о себе
    2. Задний фон

--- src/index.js
+++ src/index.js
@@ -34,6 +34,12 @@ const initElements = () => {
   const elements = [
     {
       tag: 'div',
+      className: 'background',
+      innerHtml: '',
+      onclick: null
+    },
+    {
+      tag: 'div',
       className: 'button button__up button__clicked',
       innerHtml: '',
       onclick: (e)=>clickHandler(e)
@@ -41,7 +47,71 @@ const initElements = () => {
     {
       tag: 'div',
       className: 'content',
-      innerHtml: '',
+      innerHtml: `
+        <div class='text'>
+          <a class='text__syntax'>  const                             </a>
+          <a class='text__object'>  portfolio                         </a>
+          <a class='text__syntax'>  = {                               </a>
+          <br/>
+
+          &ensp;
+          <a class='text__props'>   name                              </a>
+          <a class='text__syntax'>  :                                 </a>
+          <a class='text__str'>     'Viktor'                          </a>
+          <a class='text__syntax'>  ,                                 </a>
+          <br/>
+
+          &ensp;
+          <a class='text__props'>   ages                              </a>
+          <a class='text__syntax'>  :                                 </a>
+          <a class='text__int'>     25                                </a>
+          <a class='text__syntax'>  ,                                 </a>
+          <br/>
+
+          &ensp;
+          <a class='text__props'>   from                              </a>
+          <a class='text__syntax'>  :                                 </a>
+          <a class='text__str'>     'Yekaterinburg(RU)'               </a>
+          <a class='text__syntax'>  ,                                 </a>
+          <br/>
+
+          &ensp;
+          <a class='text__props'>   stack                             </a>
+          <a class='text__syntax'>  : [                               </a>
+          <br/>
+
+          &ensp;&ensp;
+          <a class='text__str'>     'html'                            </a>
+          <a class='text__syntax'>  ,                                 </a>
+          <br/>
+
+          &ensp;&ensp;
+          <a class='text__str'>     'css'                             </a>
+          <a class='text__syntax'>  ,                                 </a>
+          <br/>
+
+          &ensp;&ensp;
+          <a class='text__str'>     'javascript'                      </a>
+          <a class='text__syntax'>  ,                                 </a>
+          <br/>
+
+          &ensp;&ensp;
+          <a class='text__str'>     'react'                           </a>
+          <br/>
+
+          &ensp;
+          <a class='text__syntax'>  ] ,                               </a>
+          <br/>
+
+          &ensp;
+          <a class='text__props'>   mail                              </a>
+          <a class='text__syntax'>  :                                 </a>
+          <a class='text__link'>    'viktor.krasnorutsky@yandex.ru'   </a>
+          <br/>
+
+          <a class='text__syntax'>  };                                </a>
+        </div>
+      `,
       onclick: null
     },
     {
