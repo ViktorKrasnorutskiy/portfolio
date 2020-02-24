6e77d5ac9643631f6d32d30b224b4ebbd77436de
ViktorKrasnorutskiy <viktor.krasnorutskiy@yandex.ru>
2020-02-25 02:56:44 +0500

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,188 +1,171 @@
-/* Структура сайта :
-
-/home
-  content_home
-  btn_down
-
-/examples
-  btn_up
-  content_examples
-    example_card       example_card       example_card
-      title              title              title
-      btn_open           btn_open           btn_open
-
-/examples/<example>
-  background
-  btn_close
-  content_example
-    example
-*/
-const contents = {
+const CONTENT = {
   about: `
-    <div class='text'>
-      <a class='text__syntax'>  const                             </a>
-      <a class='text__object'>  portfolio                         </a>
-      <a class='text__syntax'>  = {                               </a>
-      <br/>
+      <div class='text'>
+        <a class='text__syntax'>  const                             </a>
+        <a class='text__object'>  portfolio                         </a>
+        <a class='text__syntax'>  = {                               </a>
+        <br/>
 
-      &ensp;
-      <a class='text__props'>   name                              </a>
-      <a class='text__syntax'>  :                                 </a>
-      <a class='text__str'>     'Viktor'                          </a>
-      <a class='text__syntax'>  ,                                 </a>
-      <br/>
+        &ensp;
+        <a class='text__props'>   name                              </a>
+        <a class='text__syntax'>  :                                 </a>
+        <a class='text__str'>     'Viktor'                          </a>
+        <a class='text__syntax'>  ,                                 </a>
+        <br/>
 
-      &ensp;
-      <a class='text__props'>   ages                              </a>
-      <a class='text__syntax'>  :                                 </a>
-      <a class='text__int'>     25                                </a>
-      <a class='text__syntax'>  ,                                 </a>
-      <br/>
+        &ensp;
+        <a class='text__props'>   ages                              </a>
+        <a class='text__syntax'>  :                                 </a>
+        <a class='text__int'>     25                                </a>
+        <a class='text__syntax'>  ,                                 </a>
+        <br/>
 
-      &ensp;
-      <a class='text__props'>   from                              </a>
-      <a class='text__syntax'>  :                                 </a>
-      <a class='text__str'>     'Yekaterinburg(RU)'               </a>
-      <a class='text__syntax'>  ,                                 </a>
-      <br/>
+        &ensp;
+        <a class='text__props'>   from                              </a>
+        <a class='text__syntax'>  :                                 </a>
+        <a class='text__str'>     'Yekaterinburg(RU)'               </a>
+        <a class='text__syntax'>  ,                                 </a>
+        <br/>
 
-      &ensp;
-      <a class='text__props'>   stack                             </a>
-      <a class='text__syntax'>  : [                               </a>
-      <br/>
+        &ensp;
+        <a class='text__props'>   stack                             </a>
+        <a class='text__syntax'>  : [                               </a>
+        <br/>
 
-      &ensp;&ensp;
-      <a class='text__str'>     'html'                            </a>
-      <a class='text__syntax'>  ,                                 </a>
-      <br/>
+        &ensp;&ensp;
+        <a class='text__str'>     'html'                            </a>
+        <a class='text__syntax'>  ,                                 </a>
+        <br/>
 
-      &ensp;&ensp;
-      <a class='text__str'>     'css'                             </a>
-      <a class='text__syntax'>  ,                                 </a>
-      <br/>
+        &ensp;&ensp;
+        <a class='text__str'>     'css'                             </a>
+        <a class='text__syntax'>  ,                                 </a>
+        <br/>
 
-      &ensp;&ensp;
-      <a class='text__str'>     'javascript'                      </a>
-      <a class='text__syntax'>  ,                                 </a>
-      <br/>
+        &ensp;&ensp;
+        <a class='text__str'>     'javascript'                      </a>
+        <a class='text__syntax'>  ,                                 </a>
+        <br/>
 
-      &ensp;&ensp;
-      <a class='text__str'>     'react'                           </a>
-      <br/>
+        &ensp;&ensp;
+        <a class='text__str'>     'react'                           </a>
+        <br/>
 
-      &ensp;
-      <a class='text__syntax'>  ] ,                               </a>
-      <br/>
+        &ensp;
+        <a class='text__syntax'>  ] ,                               </a>
+        <br/>
 
-      &ensp;
-      <a class='text__props'>   mail                              </a>
-      <a class='text__syntax'>  :                                 </a>
-      <a class='text__link'>    'viktor.krasnorutsky@yandex.ru'   </a>
-      <br/>
+        &ensp;
+        <a class='text__props'>   mail                              </a>
+        <a class='text__syntax'>  :                                 </a>
+        <a class='text__link'>    'viktor.krasnorutsky@yandex.ru'   </a>
+        <br/>
 
-      <a class='text__syntax'>  };                                </a>
-    </div>
-  `
+        <a class='text__syntax'>  };                                </a>
+      </div>
+    `,
+  elements: [
+    {
+      className: 'background',
+      onclick: null
+    },
+    {
+      className: 'container content __mid content__home',
+      onclick: null
+    },
+    {
+      className: 'container __mid_y __left example-card example-card__off __hidden',
+      onclick: (e)=>clickHandler(e)
+    },
+    {
+      className: 'container __mid example-card example-card__off __hidden',
+      onclick: (e)=>clickHandler(e)
+    },
+    {
+      className: 'container __mid_y __right example-card example-card__off __hidden',
+      onclick: (e)=>clickHandler(e)
+    },
+    {
+      className: 'container __mid_x __up button__home __hidden',
+      onclick: (e)=>clickHandler(e)
+    },
+    {
+      className: 'container __mid_x __down button__examples __clickable',
+      onclick: (e)=>clickHandler(e)
+    },
+    {
+      className: 'container __mid_x __up button__close __hidden',
+      onclick: (e)=>clickHandler(e)
+    }
+  ]
 }
 
-const elements = [
-  {
-    tag: 'div',
-    className: 'background background__home',
-    innerHtml: '',
-    onclick: null
-  },
-  {
-    tag: 'div',
-    className: 'content content__home',
-    innerHtml: contents.about,
-    onclick: null
-  },
-  {
-    tag: 'div',
-    className: 'example-card',
-    innerHtml: '',
-    onclick: null
-  },
-  {
-    tag: 'div',
-    className: 'example-card',
-    innerHtml: '',
-    onclick: null
-  },
-  {
-    tag: 'div',
-    className: 'example-card',
-    innerHtml: '',
-    onclick: null
-  },
-  {
-    tag: 'div',
-    className: 'button button__up button__clicked',
-    innerHtml: '',
-    onclick: (e)=>clickHandler(e)
-  },
-  {
-    tag: 'div',
-    className: 'button button__down button__clickable',
-    innerHtml: '',
-    onclick: (e)=>clickHandler(e)
-  }
-];
-
-const clickHandler = ({target}) => {
-  if (target.classList.contains('button__clickable')) {
-    Array.from(document.getElementsByClassName('button')).forEach(
-      (button) => {
-        button.classList.toggle('button__clickable')
-        button.classList.toggle('button__clicked')
-      }
-    )
-
-    const content = document.querySelector('.content')
-    const background = document.querySelector('.background')
-
-    content.classList.toggle('content__home');
-    content.classList.toggle('content__examples');
-    if (content.classList.contains('content__home')) {
-      content.innerHTML = contents.about;
-      background.classList.toggle('background__home')
-      background.classList.toggle('background__examples')
-    } else if (content.classList.contains('content__examples')) {
-      content.innerHTML = '';
-      background.classList.toggle('background__home')
-      background.classList.toggle('background__examples')
-    }
-    Array.from(document.getElementsByClassName('example-card')).forEach(
-      (card, i) => {
-        switch(i) {
-          case 0:
-            card.classList.toggle('example-card__left')
-            break;
-          case 1:
-            card.classList.toggle('example-card__mid')
-            break;
-          case 2:
-            card.classList.toggle('example-card__right')
-            break;
-        }
-        card.classList.toggle('example-card__active')
-      }
-    )
-  };
-};
-
-const initElements = () => {
-  const app = document.getElementById('app');
-  const initElement = ({tag, className, innerHtml, onclick}) => {
-    const element = document.createElement(tag);
+const __initElements = () => {
+  const initElement = ({className, onclick}) => {
+    const element = document.createElement('div');
     element.className = className;
-    element.innerHTML = innerHtml;
     element.onclick = onclick;
     return element;
   };
-  elements.forEach((element) => {
+  CONTENT.elements.forEach((element) => {
     app.append(initElement(element));
   });
+  document.querySelector('.content__home').innerHTML = CONTENT.about;
 };
-initElements();
+__initElements();
+
+const dom = {
+  app: document.getElementById('app'),
+  content: document.querySelector('.content'),
+  buttons: {
+    all: document.getElementsByClassName('button'),
+    toHome: document.querySelector('.button__home'),
+    toExamples: document.querySelector('.button__examples'),
+    closeExample: document.querySelector('.button__close')
+  },
+  example_cards: {
+    all: document.getElementsByClassName('example-card')
+  }
+}
+
+const setDom = {
+  setClassesInArray: (elems, classA, classB) => {
+    Array.from(elems).forEach(
+      (elem) => {
+        elem.classList.toggle(classA)
+        elem.classList.toggle(classB)
+      }
+    )
+  },
+  setClass: (element, classA, classB) => {
+    element.classList.toggle(classA)
+    element.classList.toggle(classB)
+  }
+}
+
+const clickHandler = ({target}) => {
+  const targetHave = (anyClass) => {
+    return target.classList.contains(anyClass);
+  };
+  if (targetHave('__clickable')) {
+    if (targetHave('button__examples') || targetHave('button__home')) {
+      setDom.setClass(dom.content, 'content__home', 'content__examples');
+      setDom.setClass(dom.buttons.toHome, '__hidden', '__clickable');
+      setDom.setClass(dom.buttons.toExamples, '__hidden', '__clickable');
+      setDom.setClassesInArray(dom.example_cards.all, '__hidden', '__clickable');
+      setDom.setClassesInArray(dom.example_cards.all, 'example-card__on', 'example-card__off');
+    } else if (targetHave('example-card') || targetHave('button__close')) {
+      setDom.setClass(dom.content, 'content__examples', 'content__example');
+      setDom.setClass(dom.buttons.closeExample, '__hidden', '__clickable');
+      setDom.setClassesInArray(dom.example_cards.all, '__hidden', '__clickable');
+      setDom.setClassesInArray(dom.example_cards.all, 'example-card__on', 'example-card__off');
+    }
+    if (dom.content.classList.contains('content__home')) {
+      dom.content.innerHTML = CONTENT.about;
+    } else if (dom.content.classList.contains('content__examples')) {
+      dom.content.innerHTML = '';
+    } else if (dom.content.classList.contains('content__example')) {
+      dom.content.innerHTML = 'example was run';
+    }
+  }
+};
